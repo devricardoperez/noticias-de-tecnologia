8394461d48734036c05412ef4fa9f9735d01fa4e
Riki <prorickdev@gmail.com>
2024-05-21 21:50:43 -0300

    Actualización del código

--- main.js
+++ main.js
@@ -9,33 +9,39 @@ ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 $(document).ready(function(){
   var carousel = $(".owl-carousel");
 
-  carousel.owlCarousel({
-    items: 4,
-    margin: 10,
-    loop: false, /* desactiva el bucle */
-    nav: true,
-    video: true,
-    lazyLoad: true,
-    slideBy: 3, /* se desplaza de 3 en 3 videos */
-    responsive:{
-      0:{
-        items:1,
-        slideBy: 1
-      },
-      600:{
-        items:3,
-        slideBy: 3
-      }
-    }
-  });
-
-  // Pausar todos los videos cuando uno comienza a reproducirse
-  carousel.find('video').on('play', function() {
-    var currentVideo = this;
-    carousel.find('video').each(function() {
-      if (this !== currentVideo) {
-        this.pause();
+  if (carousel.length) { // Verifica si el elemento existe
+    carousel.owlCarousel({
+      items: 4,
+      margin: 10,
+      loop: false, // desactiva el bucle
+      nav: true,
+      video: true,
+      lazyLoad: true,
+      slideBy: 3, // se desplaza de 3 en 3 videos
+      responsive:{
+        0:{
+          items:1,
+          slideBy: 1
+        },
+        600:{
+          items:2,
+          slideBy: 2
+        },
+        1000: {
+          items: 3,
+          slideBy: 3
+        }
       }
     });
-  });
+
+    // Pausar todos los videos cuando uno comienza a reproducirse
+    carousel.find('video').on('play', function() {
+      var currentVideo = this;
+      carousel.find('video').each(function() {
+        if (this !== currentVideo) {
+          this.pause();
+        }
+      });
+    });
+  }
 });
